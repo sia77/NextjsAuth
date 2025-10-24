8212b86e82ee87a3525c20fa8e88cf9d1d97b799
Sia <siavash_p@hotmail.com>
2025-10-24 16:23:35 -0700

    Continuing with debuging

--- helpers/getDataFRomToken.ts
+++ helpers/getDataFRomToken.ts
@@ -11,13 +11,17 @@ export const getDataFromToken = (request: NextRequest) => {
 
     const token = request.headers.get("cookie")?.split("=")[1];
 
+    console.log("****Token: ", token);
+
     if (!token) throw new Error("Authentication token is missing.");
 
     const secret = process.env.TOKEN_SECRET;
+    console.log("****secret: ", secret);
     if (!secret) throw new Error("Server misconfiguration: TOKEN_SECRET missing");
 
     try {
         const decoded = jwt.verify(token, secret) as { id: string };
+        console.log("****decoded: ", decoded);
         return decoded.id;
     } catch (error) {
         console.error("getDataFromToken:", error);
